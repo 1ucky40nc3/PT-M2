c19a3702d8eda3fec2da10a7e9b540c98605fa2b
1ucky40nc3 <louisnwendler@gmail.com>
2023-03-23 20:05:35 +0100

    restructured repo

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ setup(
     description="A Grammatical Error Correction (GEC) metric based on Pretrained-based (PT-based) models",
     long_description=open("readme.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    keywords="python nlp machine-learning deep-learning",
+    keywords=[
+        "python",
+        "nlp",
+        "natural-language-processing",
+        "ml",
+        "machine-learning",
+        "deep-learning",
+        "gec",
+        "grammatical-error-correction"
+    ],
     license="MIT License",
     url="https://github.com/1ucky40nc3/PT-M2",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    include_package_data=True,
+    packages=find_packages(exclude=[]),
 )
